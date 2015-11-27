7219252c50c774161209a6c437dabf0a802caa6b
陶磊 <taolei@taoleideMacBook-Pro.local>
2015-11-28 01:58:33 +0800

    add md5 encrypt  method of util

---
+++ src/main/java/net/hehe/utils/Md5Util1.java
@@ -0,0 +1,39 @@
+package net.hehe.utils;
+
+import java.security.MessageDigest;
+
+/**
+ * author: taolei
+ * date: 15/11/28.
+ * description:
+ */
+public class Md5Util1 implements Md5Utils{
+
+    public final String encrypt(String s) {
+        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        try {
+            byte[] btInput = s.getBytes();
+            // 获得MD5摘要算法的 MessageDigest 对象
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            // 使用指定的字节更新摘要
+            mdInst.update(btInput);
+            // 获得密文
+            byte[] md = mdInst.digest();
+            // 把密文转换成十六进制的字符串形式
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+}
---
+++ src/main/java/net/hehe/utils/Md5Utils.java
@@ -0,0 +1,16 @@
+package net.hehe.utils;
+
+/**
+ * author: taolei
+ * date: 15/11/28.
+ * description:
+ */
+public interface Md5Utils {
+    /**
+     * 加密字符串
+     *
+     * @param s
+     * @return 返回加密后的md5 字符串
+     */
+    String encrypt(String s);
+}

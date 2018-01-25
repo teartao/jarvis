7a2743b80da39f4271ab2784918cef04948d6378
neotao <l.tao@htd.cn>
2018-01-25 17:48:25 +0800

    优化excel读取，解决单元格读取数字自动添加小数点问题

--- jarvis-utils/src/main/java/net/hehe/excel/SheetProcessor.java
+++ jarvis-utils/src/main/java/net/hehe/excel/SheetProcessor.java
@@ -3,22 +3,27 @@ package net.hehe.excel;
 import net.hehe.exceptions.ExcelException;
 import net.hehe.utils.ExcelUtils;
 import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Name;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.ss.util.CellRangeAddressList;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author neo·tao
  * @Date 2017/12/18
  * @Desc
  */
-public class SheetProcessor{
+public class SheetProcessor {
     private HSSFSheet sheet;
 
     SheetProcessor(HSSFSheet sheet) {
@@ -114,23 +119,23 @@ public class SheetProcessor{
         return insertSimpleRows(startRowIndex, rows);
     }
 
-    public List<List<String>> readDataFromSheet() {
-        List<List<String>> sheetData = new ArrayList<List<String>>();
+    public List<Map<String, String>> readDataFromSheet(HSSFFormulaEvaluator formulaEvaluator) {
+        List<Map<String, String>> sheetData = new ArrayList<>();
         for (int rowIndex = 0; rowIndex <= this.sheet.getLastRowNum(); rowIndex++) {
             Row xlsRow = this.sheet.getRow(rowIndex);
             if (xlsRow != null) {
-                List<String> rowData = new ArrayList<String>();
+                Map<String, String> rowData = new HashMap<>();
                 for (int colIndex = 0; colIndex <= xlsRow.getLastCellNum(); colIndex++) {
                     Cell cell = xlsRow.getCell(colIndex);
                     if (cell != null) {
-                        rowData.add(ExcelUtils.getValue(cell));
+                        rowData.put(String.valueOf(colIndex), ExcelUtils.getCellValue(cell, formulaEvaluator));
                     } else {
-                        rowData.add("");
+                        rowData.put(String.valueOf(colIndex), "");
                     }
                 }
                 sheetData.add(rowData);
             } else {
-                sheetData.add(new ArrayList<String>());
+                sheetData.add(new HashMap<String, String>());
             }
         }
         return sheetData;
@@ -138,7 +143,23 @@ public class SheetProcessor{
     }
 
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void readFromExcel() throws InvalidFormatException {
+        try {
+            File file = new File("E:/read.xls");
+            if (!file.exists()) {
+                boolean result = file.createNewFile();
+            }
+            HSSFWorkbook workbook = (HSSFWorkbook) WorkbookFactory.create(file);
+            SheetProcessor sheetProcessor = new SheetProcessor(workbook.getSheetAt(0));
+            HSSFFormulaEvaluator formulaEvaluator = new HSSFFormulaEvaluator((HSSFWorkbook) workbook);
+            List<Map<String, String>> data = sheetProcessor.readDataFromSheet(formulaEvaluator);
+            System.out.println(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeToExcel() {
         String sheetName = "sheet1";
         HSSFWorkbook workbook = ExcelFactory.createWorkbook(new String[]{sheetName});
         SheetProcessor sheetProcessor = ExcelFactory.createSheetProcessor(workbook.getSheetAt(0));
@@ -156,6 +177,56 @@ public class SheetProcessor{
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) throws InvalidFormatException {
+        readFromExcel();
+//        writeToExcel();
+        //        formulaOperation();
+    }
+
+    private static void formulaOperation() throws IOException {
+        String[] countryName = {"张新花", "赵峰", "刘丹", "黄生功", "李春楠", "马艳珍", "张建群", "赵瑞年", "井含英", "郭元维", "王文芳",
+                "段国英", "张文婷", "陈鹏英", "常发梅", "孔繁菲", "祁洪香", "韩雅楠", "范明奎", "任顺龙", "丁永乐", "马德录", "吴红英", "严进香", "史芳",
+                "林玲", "王有运", "樊有祥", "靳智慧", "马梅", "陈加贤", "李万辉", "马斌花", "李梅林", "李生丰", "刘丹丹", "杨菊清", "贾锡通", "山永信", "陈少渔",
+                "卢君", "任永慧", "窦珍香", "张国清", "李美玲", "曹艳慧", "刘永秀", "樊光芳", "侯尚梅", "罗生琳", "张海芬", "梁召贤", "谈明宝", "贾统梅", "王生萍",
+                "周泓宇", "江秀兰", "孙小青", "马占富", "吴秀萍", "马宏伟", "德吉卓么", "梁奎先", "宋伯福", "马占刚", "李国锋", "史元", "马佐辉", "李贵福", "乔世红",
+                "李晓红", "荘柳红", "戴连梅", "裴延红", "田海兵", "党忠霞", "星玉英", "张廷良", "韩国宝", "韩庆文", "赵玉莹", "马有莲", "郭丽萍", "吴秀春", "贾延章",
+                "石维丙", "李成梅", "喇成明", "解生旺", "运占花", "熊成吉", "贾生升", "景源德", "李文君", "马洪淇", "李慧文", "魏学刚", "罗长平", "胡生春", "田种兴",
+                "李满存", "石延花", "王生兰", "赵家军", "安生年", "田生花", "赵雪玲", "宋邦元", "张红梅", "刘岩莉", "钟光霖", "汪武祥", "李连发", "张雪莲", "逯进义",
+                "马花", "思春梅", "牛永水", "刘万邦", "张小兰", "魏珍", "李永梅", "刘跃峰", "杨有德", "肖正文", "马兰", "郭永清", "蔡晓燕", "孙占德", "陈文娟", "王凌云",
+                "卢世宝", "王桂梅", "宋邦宏", "李生花", "张成芳", "赵明花", "张刘贤", "赵仲慧"};
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet realSheet = workbook.createSheet("Sheet xls");
+        HSSFSheet hidden = workbook.createSheet("hidden");
+        HSSFCell cell = null;
+        for (int i = 0, length = countryName.length; i < length; i++) {
+            String name = countryName[i];
+            HSSFRow row = hidden.createRow(i);
+            cell = row.createCell(0);
+            cell.setCellValue(name);
+        }
+
+        CellRangeAddressList regions = new CellRangeAddressList(0, 0, 0, 0);
+        //获取单元格的坐标
+        System.out.println(regions.getCellRangeAddress(0).formatAsString());
+
+        Name namedCell = workbook.createName();
+        namedCell.setNameName("hidden");
+        namedCell.setRefersToFormula("hidden!A1:A" + countryName.length);
+        //加载数据,将名称为hidden的
+        DVConstraint constraint = DVConstraint.createFormulaListConstraint("hidden");
+
+        // 设置数据有效性加载在哪个单元格上,四个参数分别是：起始行、终止行、起始列、终止列
+        CellRangeAddressList addressList = new CellRangeAddressList(0, 0, 0, 0);
+        HSSFDataValidation validation = new HSSFDataValidation(addressList, constraint);
+
+        //将第二个sheet设置为隐藏
+        workbook.setSheetHidden(1, true);
+        realSheet.addValidationData(validation);
+        FileOutputStream stream = new FileOutputStream("E:\\range.xls");
+        workbook.write(stream);
+        stream.close();
     }
 
 }
--- jarvis-utils/src/main/java/net/hehe/utils/ExcelUtils.java
+++ jarvis-utils/src/main/java/net/hehe/utils/ExcelUtils.java
@@ -1,33 +1,15 @@
 package net.hehe.utils;
 
-import org.apache.poi.hssf.usermodel.HSSFDataFormatter;
+import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
 
+import java.text.SimpleDateFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ExcelUtils {
-    public static String getValue(Cell hssfCell) {
-        String result = "";
-        HSSFDataFormatter dataFormatter = new HSSFDataFormatter();
-        if (hssfCell == null) {
-            return result;
-        }
-        switch (hssfCell.getCellType()) {
-            case Cell.CELL_TYPE_BOOLEAN:
-                result = replaceBlank(String.valueOf(hssfCell.getBooleanCellValue()));
-                break;
-            case Cell.CELL_TYPE_NUMERIC:
-                result = replaceBlank(dataFormatter.formatCellValue(hssfCell));
-                break;
-            case Cell.CELL_TYPE_STRING:
-                result = replaceBlank(String.valueOf(hssfCell.getStringCellValue()));
-                break;
-            case Cell.CELL_TYPE_ERROR:
-                break;
-        }
-        return result;
-    }
 
     static String replaceBlank(String str) {
         String dest = "";
@@ -37,5 +19,33 @@ public class ExcelUtils {
             dest = m.replaceAll("");
         }
         return dest;
+    }
+
+    //未处理公式
+    public static String getCellValue(Cell cell, FormulaEvaluator formulaEvaluator) {
+        if (cell == null) {
+            return null;
+        }
+
+        switch (cell.getCellType()) {
+            case Cell.CELL_TYPE_BOOLEAN:
+                return String.valueOf(cell.getBooleanCellValue());
+            case Cell.CELL_TYPE_NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");//非线程安全
+                    return sdf.format(cell.getDateCellValue());
+                } else {
+                    cell.setCellType(HSSFCell.CELL_TYPE_STRING);
+                    return String.valueOf(cell.getStringCellValue());
+                }
+            case Cell.CELL_TYPE_STRING:
+                return cell.getRichStringCellValue().getString().trim();
+            case Cell.CELL_TYPE_FORMULA:
+                return formulaEvaluator.evaluate(cell).getStringValue();
+            case Cell.CELL_TYPE_ERROR:
+                return "";
+            default:
+                return "";
+        }
     }
 }
